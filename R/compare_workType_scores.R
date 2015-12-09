32f33eba053d949ac29a88df2db8b815e9ac949a
Jacob Malcom <jacob.w.malcom@gmail.com>
2015-12-09 07:54:31 -0500

    Add tallies of formal consults to output table.

--- R/compare_workType_scores.R
+++ R/compare_workType_scores.R
@@ -34,6 +34,8 @@ new <- data.frame(work_type=TK$Work_type,
 new$mismatch <- (TK$expect_visible != JM$expect_visible)
 mismat <- new[new$mismatch ==FALSE, ]
 
+### This is the actual data to use! ###
+base <- "/Users/jacobmalcom/Repos/Defenders/compliance_exploration/"
 dat <- read.table(paste0(base, "data/workTypes_reconcile.tab"),
                   sep="\t",
                   header=TRUE,
@@ -53,7 +55,8 @@ full$with_coord <- ifelse(full$datum != "", TRUE, FALSE)
 sum(full$with_coord, na.rm=T)
 full$with_coord <- as.factor(full$with_coord)
 
-
+##################################################
+# let's get all consultation tallies by work category
 cons_workType_tab <- table(full$work_type, full$with_coord)
 head(cons_workType_tab)
 tmp <- as.data.frame(cons_workType_tab)
@@ -63,21 +66,53 @@ cons_workType <- tmp
 head(cons_workType)
 w_coord <- cons_workType[cons_workType$with_coord == TRUE, ]
 dim(w_coord)
-dim(w_coord)
+head(w_coord)
 
 newd <- merge(dat, cons_workType, by="Work_type")
 head(newd)
 
-forsure <- newd[newd$reconcile == 1, ]
-maybe <- newd[newd$reconcile == 0.5, ]
-novis <- newd[newd$reconcile == 0, ]
+##################################################
+# let's get just the formal consultation tallies by work category
+formal <- full[full$formal_consult == "Yes", ]
+formal_workType_tab <- table(formal$work_type, formal$with_coord)
+head(formal_workType_tab)
+tmp <- as.data.frame(formal_workType_tab)
+names(tmp) <- c("Work_type", "with_coord", "N_formal")
+head(tmp)
+formal_workType <- tmp
+head(formal_workType)
+formal_w_coord <- formal_workType[formal_workType$with_coord == TRUE, ]
+dim(formal_w_coord)
 
-sum(forsure$N_consult, na.rm=TRUE)
-sum(maybe$N_consult, na.rm=TRUE)
-sum(novis$N_consult, na.rm=TRUE)
+newf <- merge(dat, formal_workType, by="Work_type")
+head(newf)
 
-write.table(newd,
-            paste0(base, "data/joined_w_Nconsults.tab"),
+##################################################
+# combine the all and formal dfs
+all_sub <- newd[, c(1,5,6)]
+final <- merge(all_sub, newf, by=c("Work_type", "with_coord"))
+head(final)
+names(final)
+
+forsure <- final[final$reconcile == 1, ]
+maybe <- final[final$reconcile == 0.5, ]
+novis <- final[final$reconcile == 0, ]
+
+n_forsure <- sum(forsure$N_consult, na.rm=TRUE)
+n_maybe <- sum(maybe$N_consult, na.rm=TRUE)
+n_novis <- sum(novis$N_consult, na.rm=TRUE)
+all_tally <- c(n_forsure, n_maybe, n_novis)
+
+f_forsure <- sum(forsure$N_formal, na.rm=TRUE)
+f_maybe <- sum(maybe$N_formal, na.rm=TRUE)
+f_novis <- sum(novis$N_formal, na.rm=TRUE)
+form_tally <- c(f_forsure, f_maybe, f_novis)
+
+cats <- c("for sure", "maybe", "no vis.")
+tally <- data.frame(cats, all_tally, form_tally)
+
+write.table(final,
+            paste0(base, "data/joined_w_Nconsults_Nformal.tab"),
             sep="\t",
             quote=FALSE,
             row.names=FALSE)
