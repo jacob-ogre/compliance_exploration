a4da1a452104538dd6c0b2b2974a8f0de850f5c9
Jacob Malcom <jmalcom@defenders.org>
2017-03-15 13:47:59 -0400

    More changes to convert to a pkg with a vignette.

--- inst/R/analyze_eval_data.R
+++ inst/R/analyze_eval_data.R
@@ -1,6 +1,23 @@
 # BSD_2_clause
 # Analysis of the monitoring data.
 
+---
+title: "ESA compliance with remotely sensed data"
+author: "Tiffany Kim, Ya-Wei Li, and Jacob Malcom"
+date: "`r Sys.Date()`"
+output:
+  rmarkdown::html_document:
+  code_folding: hide
+  css: custom.css
+  df_print: kable
+  fig_caption: yes
+  fig_width: 7
+  highlight: tango
+  toc: true
+  toc_depth: 3
+  toc_float: true
+---
+
 library(dplyr)
 library(ggplot2)
 library(ggrepel)
@@ -14,87 +31,6 @@ library(stringr)
 library(tidyr)
 
 source("multiplot.R")
-
-###############################################################################
-# Load the data and prep
-base <- "/Users/jacobmalcom/Work/Repos/Defenders/analyses/compliance_exploration"
-form_eval <- paste0(base, "/data/remote_sensing_monitor_evaluation.xlsx")
-inform_eval <- paste0(base, "/data/remote_sensing_eval_informal.xlsx")
-form_consults <- paste0(base, "/data/random_sample_formal_consults_w_decdeg.tab")
-inform_consults <- paste0(base, "/data/random_sample_informal_consults_w_decdeg.tab")
-expected_f <- paste0(base, "/data/joined_w_Nconsults_Nformal.tab")
-
-form <- read_excel(form_eval, sheet = 1)
-inform <- read_excel(inform_eval, sheet = 1)
-form_cons <- read.table(form_consults,
-                        header = T,
-                        sep = "\t",
-                        stringsAsFactors = F)
-inform_cons <- read.table(inform_consults,
-                          header = T,
-                          sep = "\t",
-                          stringsAsFactors = F)
-expect <- read.table(expected_f, header = T, sep = "\t", stringsAsFactors = F)
-
-names(form)
-names(inform)
-names(form_cons)
-names(inform_cons)
-names(expect)
-
-# Winnow to just the rows with data
-form <- form[1:sum(!is.na(form$activity_code)), ]
-inform <- inform[1:sum(!is.na(form$activity_code)), ]
-expect <- expect[expect$with_coord == TRUE, ]
-
-###############################################################################
-# Formal consultations
-#
-# Do the first join to get consult data
-form_1 <- left_join(form, form_cons, by = "activity_code")
-# dim(form_1)
-# names(form_1)
-form_1 <- form_1[, c(1:12, 15:20, 23:27, 34:36, 49)]
-# names(form_1)
-
-# Do the second join to get our expectations
-form_dat <- left_join(form_1, expect, by = c("work_type" = "Work_type"))
-names(form_dat)[2] <- "action_found"
-
-# Informal consultations
-#
-# Do the first join to get consult data for informal consults
-dim(inform)
-inform_1 <- left_join(inform, inform_cons, by = "activity_code")
-dim(inform_1)
-glimpse(inform_1)
-names(inform_1)
-inform_1 <- inform_1[, c(1:12, 15:20, 23:27, 34:36, 49)]
-names(inform_1)
-
-# Do the second join to get our expectations
-inform_dat <- left_join(inform_1, expect, by = c("work_type" = "Work_type"))
-names(inform_dat)[2] <- "action_found"
-
-# some type conversions for formal and informal
-form_dat$area <- as.numeric(form_dat$area)
-inform_dat$area <- as.numeric(inform_dat$area)
-form_dat$start_date <- mdy(form_dat$start_date)
-inform_dat$start_date <- mdy(inform_dat$start_date)
-form_dat$FWS_concl_date <- mdy(form_dat$FWS_concl_date)
-inform_dat$FWS_concl_date <- mdy(inform_dat$FWS_concl_date)
-
-# make a combined df
-names(inform_dat)[9] <- "completed"
-form_dat$formal_in <- rep("formal", length(form_dat$activity_code))
-inform_dat$formal_in <- rep("informal", length(inform_dat$activity_code))
-combo_dat <- rbind(form_dat, inform_dat)
-
-write_tsv(combo_dat,
-          file.path(base, "data", "merged_formal_informal_data.tsv"))
-
-saveRDS(combo_dat,
-        file.path(base, "data", "merged_formal_informal_data.rds"))
 
 ###############################################################################
 # Let's do some plotting and analysis!
